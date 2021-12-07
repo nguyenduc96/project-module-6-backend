1adf630157885c0c1b9073c565d1197004187620
Le-Anh-Tu <leanhtu280295@gmail.com>
2021-12-07 10:12:39 +0700

    tu

--- src/main/java/com/diosa/repository/IUserRepository.java
+++ src/main/java/com/diosa/repository/IUserRepository.java
@@ -1,14 +1,19 @@
 package com.diosa.repository;
 
+import com.diosa.model.project.Project;
 import com.diosa.model.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import javax.transaction.Transactional;
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface IUserRepository extends JpaRepository<User, Long> {
     @Transactional
     Optional<User> findByUsername(String username);
+
+
 }
