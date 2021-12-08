115403a290a181ff1b7e52873fbd3de08bc80aeb
Your Name <danganh28597@gmail.com>
2021-12-08 07:56:59 +0700

    done comment BE

--- src/main/java/com/diosa/controller/CommentController.java
+++ src/main/java/com/diosa/controller/CommentController.java
@@ -1,13 +1,22 @@
 package com.diosa.controller;
 
 import com.diosa.model.comment.Comment;
+import com.diosa.model.task.Task;
+import com.diosa.model.user.User;
+import com.diosa.model.user.UserPrinciple;
 import com.diosa.service.comment.CommentService;
 import com.diosa.service.comment.ICommentService;
+import com.diosa.service.user.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
+import java.text.Format;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
 import java.util.Optional;
 @CrossOrigin("*")
 @RestController
@@ -17,6 +26,9 @@ public class CommentController {
     @Autowired
     private ICommentService commentService;
 
+    @Autowired
+    private IUserService userService;
+
 
     @GetMapping
     public ResponseEntity<Iterable<Comment>> showComment(){
@@ -24,8 +36,18 @@ public class CommentController {
     }
 
     @PostMapping
-    public ResponseEntity<Comment> AddComment(Comment comment) {
+    public ResponseEntity<Comment> AddComment(@RequestBody Comment comment, Authentication authentication) {
+        UserPrinciple userPrinciple = (UserPrinciple) authentication.getPrincipal();
+        User user = new User();
+        user.setId(userPrinciple.getId());
+        comment.setDate(convertDateToString(new Date()));
+        comment.setUser(user);
         return new ResponseEntity<>(commentService.save(comment),HttpStatus.CREATED);
+    }
+
+    private String convertDateToString(Date date) {
+        Format formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm");
+        return formatter.format(date);
     }
 
     @DeleteMapping("/{id}")
@@ -38,5 +60,15 @@ public class CommentController {
         return new ResponseEntity<>(HttpStatus.OK);
 
     }
+
+    @GetMapping("/task/{taskId}")
+    public ResponseEntity<List<Comment>> findByTaskId(@PathVariable Long taskId) {
+        return new ResponseEntity<>(commentService.findAllByTaskId(taskId), HttpStatus.OK);
+    }
+
+//    @GetMapping("/{id}")
+//    public ResponseEntity<Comment> findById(@PathVariable Long id){
+// khong can
+//    }
 
 }
